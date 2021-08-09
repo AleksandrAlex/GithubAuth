636b43f2483aa449e326b5cbc5a5a62f12206271
Aleksandr Suslov <suslovalmi@mail.ru>
2021-08-09 19:17:03 +0300

    Add navigation drawer

--- app/src/main/java/com/example/githubauthorization/presentation/MainActivity.kt
+++ app/src/main/java/com/example/githubauthorization/presentation/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.githubauthorization.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.githubauthorization.R
+import com.example.githubauthorization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        binding.navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) ||
+        super.onSupportNavigateUp()
     }
 }
